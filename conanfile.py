4f18af0bb68855c10dd5a00e6261934cec3b8610
Julian Thijssen <julianthijssen@gmail.com>
2026-09-12 00:59:43 +0200

    Remove Release build

--- conanfile.py
+++ conanfile.py
@@ -123,12 +123,12 @@ class CellMorphologyDataConan(ConanFile):
 
         cmake = self._configure_cmake()
         cmake.build(build_type="RelWithDebInfo")
-        cmake.build(build_type="Release")
+        #cmake.build(build_type="Release")
 
     def package(self):
         package_dir = pathlib.Path(self.build_folder, "package")
         relWithDebInfo_dir = package_dir / "RelWithDebInfo"
-        release_dir = package_dir / "Release"
+        
         print("Packaging install dir: ", package_dir)
         subprocess.run(
             [
@@ -141,17 +141,7 @@ class CellMorphologyDataConan(ConanFile):
                 relWithDebInfo_dir,
             ]
         )
-        subprocess.run(
-            [
-                "cmake",
-                "--install",
-                self.build_folder,
-                "--config",
-                "Release",
-                "--prefix",
-                release_dir,
-            ]
-        )
+
         self.copy(pattern="*", src=package_dir)
 
     def package_id(self):
@@ -161,6 +151,6 @@ class CellMorphologyDataConan(ConanFile):
         self.cpp_info.relwithdebinfo.libdirs = ["RelWithDebInfo/lib"]
         self.cpp_info.relwithdebinfo.bindirs = ["RelWithDebInfo/Plugins", "RelWithDebInfo"]
         self.cpp_info.relwithdebinfo.includedirs = ["RelWithDebInfo/include", "RelWithDebInfo"]
-        self.cpp_info.release.libdirs = ["Release/lib"]
-        self.cpp_info.release.bindirs = ["Release/Plugins", "Release"]
-        self.cpp_info.release.includedirs = ["Release/include", "Release"]
+        #self.cpp_info.release.libdirs = ["Release/lib"]
+        #self.cpp_info.release.bindirs = ["Release/Plugins", "Release"]
+        #self.cpp_info.release.includedirs = ["Release/include", "Release"]
